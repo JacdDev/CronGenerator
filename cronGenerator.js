de1ddb1894828323b06673be715188a3c1725d51
Jose <joseantonio.caravacadiosdado@gmail.com>
2019-09-10 13:53:45 +0200

    update constructor

--- cronGenerator.js
+++ cronGenerator.js
@@ -1,7 +1,12 @@
 (function($) {
 
-    function makeConfigurator(options){
-
+    //TODO create structure of visual generator
+    function makeConfigurator(cronGenerator,options){
+$('<div class="tab">').appendTo(cronGenerator);
+$('<button class="tablinks" onclick="openTab(event, ""seconds"")">Seconds</button>').appendTo(cronGenerator);
+$('<button class="tablinks" onclick="openCity(event, ""minutes"")">Minutes</button>').appendTo(cronGenerator);
+$('<button class="tablinks" onclick="openCity(event, ""Tokyo"")">Hours</button>').appendTo(cronGenerator);
+$('</div>').appendTo(cronGenerator);
     }
 
 	var defaultOptions = {
@@ -31,36 +36,49 @@
         onChange: undefined
 	};
 
-    var methods = {
-        init : function(opts) {
+    function CronGenerator($this, opts) {
 
-            //initialize options with user options and default options
-            var options = opts ? opts : {};
-            var allOptions = $.extend([], defaultOptions, options);
-            $.extend(allOptions, {
-                seconds     : $.extend({}, defaultOptions.seconds, allOptions, options.seconds),
-                minutes     : $.extend({}, defaultOptions.minutes, allOptions, options.minutes),
-                hours       : $.extend({}, defaultOptions.hours, allOptions, options.hours),
-                days        : $.extend({}, defaultOptions.days, allOptions, options.days),
-                daysOfWeek  : $.extend({}, defaultOptions.daysOfWeek, allOptions, options.daysOfWeek),
-                months      : $.extend({}, defaultOptions.months, allOptions, options.months),
-                years       : $.extend({}, defaultOptions.years, allOptions, options.years),
-            });
+        //initialize options with user options and default options
+        var options = opts ? opts : {};
+        var allOptions = $.extend([], defaultOptions, options);
+        $.extend(allOptions, {
+            seconds     : $.extend({}, defaultOptions.seconds, allOptions, options.seconds),
+            minutes     : $.extend({}, defaultOptions.minutes, allOptions, options.minutes),
+            hours       : $.extend({}, defaultOptions.hours, allOptions, options.hours),
+            days        : $.extend({}, defaultOptions.days, allOptions, options.days),
+            daysOfWeek  : $.extend({}, defaultOptions.daysOfWeek, allOptions, options.daysOfWeek),
+            months      : $.extend({}, defaultOptions.months, allOptions, options.months),
+            years       : $.extend({}, defaultOptions.years, allOptions, options.years),
+        });
 
-			this.value=allOptions.initial;
+        this.element = $this;
+        this.value=allOptions.initial;
 
-			this.getValue = function getValue(){
-				return this.value;
-			}
-
-            makeConfigurator(allOptions);
-
-			return this;
-        }
+        //makeConfigurator(this,allOptions);
     };
 
-    $.fn.cronGenerator = function(method) {
-        return methods.init.apply(this, arguments);
+    CronGenerator.prototype.getValue = function () {
+        return this.value;
+    };
+
+    CronGenerator.prototype.getElement = function () {
+        return this.element;
+    };
+
+    //Returns the CronGenerator associated to element
+    //If many elements are passed, it returns a list of cronGenerators
+    $.fn.cronGenerator = function(options) {
+        cronGenerators = [];
+
+        this.each(function(){
+            cronGenerators.push(new CronGenerator(this, options));
+        });
+
+        //TODO retuns always the list?
+        if(cronGenerators.length==1)
+            return cronGenerators[0];
+        else
+            return cronGenerators;
     };
 
 })(jQuery);
